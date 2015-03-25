cf106d82f28b2b21ab949252ddd513d6b9e55722
wp132322 <tmdwns2536@naver.com>
2015-03-25 14:12:17 +0900

    고속도로 실습과제

--- Javapractice/src/org/dimigo/basic/Condition.java
+++ Javapractice/src/org/dimigo/basic/Condition.java
@@ -5,31 +5,31 @@ public class Condition {
 	public static void main(String[] args) {
 		
 		String carname = "고속버스";
-		int cartype, sum = 0;
-		int dis = 25;
+		int cartype, pay = 0;
+		int dis = 20;
 	
 		
 		switch (carname){
 		case "고속버스":
-			sum	 = 850;
+			pay	 = 850;
 			cartype = 300;
 			break;
 		case "경차":
-		sum = 300;
+		pay = 300;
 		cartype = 200;
 		break;
 		
 		default:
-			sum = 600;
+			pay = 600;
 		cartype = 200;
 		break;
-		}
+		}		
 		
 		if(dis % 10==0){
-				sum += (cartype*((dis/10)-1));
+				pay += (cartype*((dis/10)-1));
 		}
 		else{
-			sum +=(cartype *(dis/10));
+			pay +=(cartype *(dis/10));
 			
 		}
 		System.out.println("<<고속도로 통행료 계산>>");
@@ -38,7 +38,7 @@ public class Condition {
 		
 		System.out.println("차종 : " + carname);
 		
-		System.out.println("통행료:" + sum +"원");
+		System.out.println("통행료:" + pay +"원");
 	}
 
 }
