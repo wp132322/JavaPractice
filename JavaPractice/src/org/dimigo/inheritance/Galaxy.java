0d60d7e6ceb0e4251b3fbcd9284c41d5c9a83d67
wp132322 <tmdwns2536@naver.com>
2015-08-28 14:39:45 +0900

    휴대폰

---
+++ JavaPractice/src/org/dimigo/inheritance/Galaxy.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ *org.dimigo.inheritance
+ *	|_ Galaxy 
+ *
+ *1. 개요:
+ *2. 작성일 : 2015. 8. 28.
+ * </pre>
+ * @author	: pc1
+ * @version	: 1.0
+ */
+public class Galaxy extends SmartPhone{
+	Galaxy()
+	{
+		
+	}
+	
+	Galaxy(String model, String company, int price)
+	{
+		super(model, company, price);
+	}
+	
+	public void pay()
+	{
+		System.out.print("삼성 페이로 ");
+		super.pay();
+	}
+	
+	public void useWirelessCharging()
+	{
+		System.out.println("무선충전기능을 사용합니다.");
+	}
+
+}
---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhone.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ *org.dimigo.inheritance
+ *	|_ SmartPhone 
+ *
+ *1. 개요:
+ *2. 작성일 : 2015. 8. 28.
+ * </pre>
+ * @author	: pc1
+ * @version	: 1.0
+ */
+public class SmartPhone {
+	private String model;
+	private String company;
+	private int price;
+	
+	SmartPhone(){
+		
+	}
+	SmartPhone (String model, String company, int price)
+	{
+		this.model = model;
+		this.company = company;
+		this.price = price;
+	}
+	public void turnOn()
+	{
+		System.out.println(model + "전원을 켭니다");
+	}
+	public void turnOff() {
+		System.out.println(model +"전원을 끕니다");
+	}
+	public void pay()
+	{
+		System.out.println("결제합니다");
+	}
+	public void useSpecialFunction(SmartPhone phone)
+	{
+		if(phone == null) return;
+		if(phone instanceof IPhone) ((IPhone)phone).useAirDrop();
+		if(phone instanceof Galaxy) ((Galaxy)phone).useWirelessCharging();
+	}
+	@Override
+	public String toString() {
+		return "모델명 : " + model + ", 제조사 : " + company + ", 가격 : " + String.format("%,d", price);
+	}
+}
+				
+	
+	
+	
+	
+
+
